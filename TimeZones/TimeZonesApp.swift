27f0e78bd0a88794d564321a7031fba9035dcc1a
Max <maxim.macari.dam@gmail.com>
2020-10-07 03:27:28 +0200

    Initial Commit

---
+++ TimeZones/TimeZonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeZonesApp.swift
+//  TimeZones
+//
+//  Created by Maxim Macari on 07/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct TimeZonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
